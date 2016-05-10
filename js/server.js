4b1649470659b7f4ed17cb81902916d8195d84bd
Jace Briggs <bri08006@byui.edu>
2016-05-10 16:29:12 -0600

    did a lot of css and the main page is pretty much styled

--- js/server.js
+++ js/server.js
@@ -1,3 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
+mongoose.connect('mongofdb://localhost/contestData')
+mongoose.connection.once(open), function()
