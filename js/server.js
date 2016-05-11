11ef51ebdfa5cf156b8e6488d84f1efff11009cd
Jace Briggs <bri08006@byui.edu>
2016-05-10 23:53:36 -0600

    more stuff

--- js/server.js
+++ js/server.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var port = 4000;
 
 mongoose.connect('mongofdb://localhost/contestData')
-mongoose.connection.once(open), function()
+mongoose.connection.once(open), function(){
+  console.log("connecting to yoyo database");
+}
+
+app.listen(port, function(){
+  console.log("Easy listening on port " + port ": the breeze");
+})
